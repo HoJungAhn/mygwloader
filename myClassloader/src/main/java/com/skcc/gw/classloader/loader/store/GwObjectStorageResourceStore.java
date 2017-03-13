5183e368180456852e5dd134bebcfbde8faa1eb9
ahnhojung <ahnhojung@10.250.66.189>
2017-03-13 11:34:26 +0900

    .

--- myClassloader/src/main/java/com/skcc/gw/classloader/loader/store/GwObjectStorageResourceStore.java
+++ myClassloader/src/main/java/com/skcc/gw/classloader/loader/store/GwObjectStorageResourceStore.java
@@ -1,66 +1,26 @@
 package com.skcc.gw.classloader.loader.store;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-
-import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class GwObjectStorageResourceStore implements GwResouceStore{
 	private Logger logger = LoggerFactory.getLogger(GwObjectStorageResourceStore.class);
-	private File rootPath;
-	
-	public GwObjectStorageResourceStore(String rootPath){
-		this.rootPath = new File(rootPath);
+
+	public void write(String pResourceName, byte[] pResourceData) {
+		// TODO Auto-generated method stub
+		
 	}
-	
-	public GwObjectStorageResourceStore(File rootPath){
-		this.rootPath = rootPath;
-	}
-	
+
 	public byte[] read(String pResourceName) {
-        InputStream is = null;
-        try {
-            is = new FileInputStream(getFile(pResourceName));
-            final byte[] data = IOUtils.toByteArray(is);
-            return data;
-        } catch (Exception e) {
-        	logger.error("can not read file :" + pResourceName, e);
-            return null;
-        } finally {
-            IOUtils.closeQuietly(is);
-        }
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	public void remove(String pResourceName) {
-		getFile(pResourceName).delete();
-	}
-
-	public void write(String pResourceName, byte[] pResourceData) {
-		FileOutputStream fout = null;
-		
-		try{
-			File file = getFile(pResourceName);
-			if(file.getParentFile().exists() == false){
-				file.getParentFile().mkdirs();
-			}
-			fout = new FileOutputStream(file);
-			IOUtils.write(pResourceData, fout);
-		}catch(IOException e){
-			logger.error("can not write file", e);
-		}finally{
-			IOUtils.closeQuietly(fout);
-		}
+		// TODO Auto-generated method stub
 		
 	}
 	
-    private File getFile( String pResourceName ) {
-        final String fileName = pResourceName.replace('/', File.separatorChar);
-        return new File(rootPath, fileName);
-    }
+
 
 }
